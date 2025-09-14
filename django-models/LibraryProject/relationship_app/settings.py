1e879c18621dfeaad575d8d58ad24235429ef871
AbaynehAlemayehu <abneh21@gmail.com>
2025-09-14 17:44:54 +0300

    add

--- django-models/LibraryProject/relationship_app/settings.py
+++ django-models/LibraryProject/relationship_app/settings.py
@@ -140,3 +140,18 @@ INSTALLED_APPS = [
     # Your app
     'relationship_app',
 ]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],  # leave empty
+        'APP_DIRS': True,  # MUST be True
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -16,3 +16,10 @@ class LibraryDetailView(DetailView):
     context_object_name = "library"
 
 # Create your views here.
+    from django.shortcuts import render
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
