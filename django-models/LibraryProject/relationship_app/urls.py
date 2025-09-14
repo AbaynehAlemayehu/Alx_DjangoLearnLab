059dec389bee155d31def78d33bdb2eadbff5955
AbaynehAlemayehu <abneh21@gmail.com>
2025-09-14 17:47:59 +0300

    add

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,10 +20,9 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
+    path('books/', views.list_books, name='list_books'),
 ]
