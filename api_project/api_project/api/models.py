d0a8f30f1b2f61efbf3f785857313918080c28e9
AbaynehAlemayehu <abneh21@gmail.com>
2025-09-27 14:10:15 +0300

    ADD

--- api_project/api_project/api/models.py
+++ api_project/api_project/api/models.py
@@ -9,3 +9,11 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
