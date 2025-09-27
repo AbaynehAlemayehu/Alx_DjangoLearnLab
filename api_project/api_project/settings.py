9cfc2c8813022c2bafc996e488fb619f443faf6f
AbaynehAlemayehu <abneh21@gmail.com>
2025-09-27 15:18:49 +0300

    Add

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -161,4 +161,18 @@ INSTALLED_APPS = [
     # Local
     'api',
 ]
+INSTALLED_APPS = [
+    ...
+    'rest_framework',
+    'rest_framework.authtoken',  # add this
+    'api',
+]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
